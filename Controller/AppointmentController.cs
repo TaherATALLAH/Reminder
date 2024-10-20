8bd4a10c4c2b68996ff6dab99ca0f19aed261f6f
taher atallah <atallahtaher@outlook.fr>
2024-10-20 12:35:24 +0200

    init call callendly and ignore files from git

---
+++ Controller/AppointmentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reminder.Domain.Interfaces;
+
+namespace Reminder.Application.Controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly ICalendlyService _calendlyService;
+
+        public AppointmentController(ICalendlyService calendlyService)
+        {
+            _calendlyService = calendlyService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAppointmentsAsync()
+        {
+            var result = await _calendlyService.GetAppointmentsAsync();
+            return Ok(result);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using Reminder.Domain.Interfaces;
 using Reminder.Infrastructure.Entity;
+using Reminder.Infrastructure.Services;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.AddDbContext<ReminderDbContext>(options =>
     options.UseSqlite("Data Source= reminder.db"));
+builder.Services.AddHttpClient<ICalendlyService, CalendlyService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
